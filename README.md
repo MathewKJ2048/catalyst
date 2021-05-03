f65a6f362615a41ce25915f0a99ee66ea9f72cd1
Nancy Day <nday@uwaterloo.ca>
2021-05-03 09:23:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Alloy Models
+
+Alloy models is a repository of Alloy models and contains scripts for scraping Alloy models and filtering these sets for particular characteristics.
+
+## Using the Model Sets
+
+The directory "models-sets" contains sets of Alloy models (sometimes within the directory hierarchy they came with) created for various purposes.  Each directory is dated by the date is was created.  The README.md file within with model set directory tells you what filters (programmatically or manually) to create this model set.
+
+## Building a Model Set Using the Scripts
+
+### Program the filters in the Script
+
+See src/main/java/alloymodeltools/alloymodeltools.java and choose the sources and filters you want to use to create a new model set.
+
+### Method 1: Running the Script directly using gradle
+
+You can run the script using "./gradlew run"
+
+### Method 2: Separate Compilation and Run
+
+#### Building the Script
+
+In the root directory, run "./gradlew build".   This results in "build/libs/allow-model-sets-all.jar" (which is a fat jar built by shadowJar).
+
+#### Running the Scripts
+
+Run "java -jar build/libs/allow-model-sets-all.jar"
+
+This will output diagnostic information and create your new model set in "models-sets/date-time/".  Within that directory there will be an initial README.md file that shows how the set was created programmatically.  You may wish to do more filtering and add to the README.md the effects of your own (manual or otherwise) filters.
+
---
+++ src/main/java/alloymodelsettools/AlloyModelSetTools.java
@@ -0,0 +1,200 @@
+/*
+ * Catalyst -- A framework for performance analysis/optimization of Alloy models
+ * Copyright (C) 2018-2019 Amin Bandali
+ * Modified 2021 Nancy Day to become AlloyModelSetTools
+ *
+ * This file is part of Catalyst.
+ *
+ * Catalyst is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Catalyst is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Catalyst.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package alloymodelsettools;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
+import org.kohsuke.github.*;
+import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_256;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.binary.Base32;
+
+// We should probably have a constant for the max number of repos to clone?
+// and how many models to find
+
+public class AlloyModelSetTools {
+
+	static FileWriter readmefile;
+	static String dirname;
+	// stdio is used for error output
+
+    static String sha256(String s) {
+        return new Base32().encodeAsString(new DigestUtils(SHA_256).digest(s));
+    }
+
+    static String sha256_n(String s, int n) {
+        return sha256(s).substring(0, n).toLowerCase();
+    }
+
+    static String sha256_32(String s) {
+        return sha256_n(s, 32);
+    }
+
+    // separate this into different functions
+    // each function to add something to the README.md file of the new model-set directory
+    // gather: from github
+    // gather: from existing model-sets (might be ones others added in here)
+    // filter: remove non-Alloy files 
+    // filter: remove duplicate models
+    // filter: remove Alloy util models
+    // filter: randomly choose one model from each directory
+    // filter: remove those that don't compile in Alloy
+    // fcn: choose scopes for a model and record these in a .csv
+    // fcn: run alloy analyzer on models with certain scopes
+    // filter: ones that have transitive closure or other syntactic features
+    // filter: models that are SAT/UNSAT in kodkod
+
+    static Integer CreateModelSetDir() {
+    	try {
+	    	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");  
+	   		LocalDateTime now = LocalDateTime.now();
+	   		dirname = "model-sets/"+ dtf.format(now); 
+	   		String readmefilename =  dirname + "/" + "README.md";
+
+			File f = new File(readmefilename);
+			f.getParentFile().mkdirs();
+	    	readmefile = new FileWriter(readmefilename);	    	
+	    	readmefile.write("Model set created: "+dirname+"\n");
+	    	return 0;
+	    } catch (Exception e) {
+	    	System.out.println("An error occurred creating the model set directory and/or README.md file");
+	    	return 1;
+	    }
+    }
+
+    static Integer GatherFromGithub() {
+
+    	String query = "language:alloy NOT ableton NOT midi NOT music NOT mIRC -repo:AlloyTools/models";
+        boolean showDescriptions = false;
+        boolean useSSHUrl = false;    // whether to use the SSH protocol for cloning, or HTTPS
+        boolean prependSHA256 = true; // whether to prepend SHA-256 of url to folder name (to obtain unique names)
+        boolean makeCloneList = true; // format output as a convenient list of "git clone" commands
+
+        makeCloneList = makeCloneList && !showDescriptions; // only make clone list if showDescriptions is false
+
+        GitHub gh;
+
+        try {
+            gh = GitHub.connect();
+        	if (gh == null) {
+            	System.out.println("Connection to GitHub failed");
+            	return 1;
+        	}
+        	System.out.println("Connected to GitHub");
+        	
+        	PagedSearchIterable<GHRepository> repos = gh.searchRepositories().q(query).list();
+
+        	System.out.println("Search query: " + query);
+        	Integer numresults = repos.getTotalCount();
+        	System.out.println("# of results: " + numresults);
+        	
+	        for (GHRepository repo : repos) {
+	            String str = makeCloneList ? "git clone" : "";
+
+	            String url = useSSHUrl ? repo.getSshUrl() : repo.getHttpTransportUrl();
+	            str += " " + url;
+
+	            if (showDescriptions)
+	                str += " " + repo.getDescription();
+	            else if (makeCloneList) {
+	                str += " ";
+	                if (prependSHA256)
+	                    str += sha256_32(url) + "-";
+	                str += repo.getName();
+	            }
+	            System.out.println(str);
+	        }
+            // Now we have a giant string of "git clone"s
+	        // NAD: can we use Java's process builder to actually do the git cloning?
+            // NAD: the following was copied from the web and isn't yet working
+/*        	ProcessBuilder pb = new ProcessBuilder(str);
+        	pb.directory(new File(dirname));
+        	pb.inheritIO();
+        	pb.redirectErrorStream(true);
+        	try {
+        		Process process = pb.start();
+        		BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            	String line;
+            	while ((line = reader.readLine()) != null) {
+                	System.out.println(line);
+            	}
+            	reader.close();
+            	int exitVal = process.waitFor();
+            	if (exitVal != 0) {
+                	System.out.println("Abnormal Behaviour! Something bad happened.");
+            	}
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}
+*/
+	        // write to readme github query used
+	        readmefile.write("Scraped from "+numresults+" github repos with query: "+query+"\n");
+	        return 0;
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    	return 1;
+	    }
+    }
+
+    static Integer GatherFromExistingModelSets(String[] dirs) {
+    	// Do not duplicate anything already in model-set directory
+    	// write to readme which directories gathered from
+    	return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    	// Start a new model set directory
+    	// expects to be run in root of alloy-models-sets directory
+    	if (CreateModelSetDir() == 1) { 
+    		return;
+    	}
+    	// Gather from github
+    	if (GatherFromGithub() == 1) {
+    		System.out.println("Failed to gather models from github");
+    		return;
+    	}
+    	// Gather from existing models-sets
+    	/*
+    	existing_model_sets_to_use = []
+    	if GatherFromExistingModelSets(existing_model_sets) == 1 {
+    		System.out.println("Failed to gather models from existing model sets")
+    		return;
+    	}
+    	*/
+    	try {
+    		readmefile.close();
+    	} catch (Exception e) {
+    		System.out.println("Failed to close readme file");
+            return;
+        }
+    	return;
+    }
+}
